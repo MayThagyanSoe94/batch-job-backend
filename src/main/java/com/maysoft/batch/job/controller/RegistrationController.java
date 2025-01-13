6d07cd4284f3d9454fed73cfb13cad7cf2d1d564
May Thagyan Soe <may.thagyan@privasia.com>
2025-01-13 20:54:41 +0800

    just add 401 message

--- src/main/java/com/maysoft/batch/job/controller/RegistrationController.java
+++ src/main/java/com/maysoft/batch/job/controller/RegistrationController.java
@@ -6,10 +6,11 @@ import com.maysoft.batch.job.service.MyUserDetailService;
 import com.maysoft.batch.job.webtoken.JwtService;
 import com.maysoft.batch.job.webtoken.LoginForm;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -39,14 +40,27 @@ public class RegistrationController {
     }
 
     @PostMapping("/authenticate")
-    public String authenticateAndGetToken(@RequestBody LoginForm loginForm) {
-        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
-                loginForm.getUsername(), loginForm.getPassword()
-        ));
-        if (authentication.isAuthenticated()) {
-            return jwtService.generateToken(myUserDetailService.loadUserByUsername(loginForm.getUsername()));
-        } else {
-            throw new UsernameNotFoundException("Invalid credentials");
+    public ResponseEntity<?> authenticateAndGetToken(@RequestBody LoginForm loginForm) {
+        try {
+            Authentication authentication = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            loginForm.getUsername(), loginForm.getPassword()
+                    )
+            );
+
+            if (authentication.isAuthenticated()) {
+                String token = jwtService.generateToken(
+                        myUserDetailService.loadUserByUsername(loginForm.getUsername())
+                );
+                return ResponseEntity.ok(token);
+            } else {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                        .body("Invalid credentials");
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body("Invalid credentials");
         }
     }
+
 }
